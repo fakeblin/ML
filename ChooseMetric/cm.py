f3e666076ec90e49ca55adf448ff7623e7e7950d
Kopyl <fakeblin@gmail.com>
2018-03-24 23:01:10 +0500

    Add Similarity-basedClassifier + ChooseMetric

---
+++ ChooseMetric/cm.py
@@ -0,0 +1,25 @@
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import scale
+from sklearn.datasets import load_boston
+import numpy as np
+
+data = load_boston()
+data['data'] = scale(data['data'])
+
+p = np.linspace(1, 10, 200)
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+file = open("grade.txt", "w")
+
+for i in p:
+    model = KNeighborsRegressor(metric='minkowski', n_neighbors=5,
+                                weights='distance', p=i)
+    # model.fit(data['data'], data['target'])
+    grade = cross_val_score(estimator=model, X=data['data'], y=data['target'],
+                            cv=kf, scoring='neg_mean_squared_error')
+
+    file.write(str(i) + ': ' + str(round(grade.mean(), 1)) + '\n')
+
+file.close()
+
---
+++ Similarity-basedClassifier/knn.py
@@ -0,0 +1,56 @@
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import scale
+import pandas
+
+data = pandas.read_csv('../wine.data', header=None)
+
+data_class = data[0]
+data_attribute = data.drop([0], axis=1)
+
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+max = 0
+k = 0;
+
+file = open("gradeBeforeScale.txt", "a")
+
+for i in range(1, 50):
+    model = KNeighborsClassifier(n_neighbors=i)
+    knn = model.fit(data_attribute, data_class)
+    grade = cross_val_score(estimator=knn, X=data_attribute, y=data_class, cv=kf)
+
+    if max < grade.mean():
+        max = grade.mean()
+        k = i
+
+
+    file.write(str(grade) + ' ' + str(round(grade.mean(), 2)) + '\n')
+
+file.close()
+
+file = open("KBeforeScale.txt", "w")
+file.write(str(k))
+file.close()
+
+data_attribute = scale(data_attribute)
+
+file = open("gradeAfterScale.txt", "a")
+
+for i in range(1, 50):
+    model = KNeighborsClassifier(n_neighbors=i)
+    knn = model.fit(data_attribute, data_class)
+    grade = cross_val_score(estimator=knn, X=data_attribute, y=data_class, cv=kf)
+
+    if max < grade.mean():
+        max = grade.mean()
+        k = i
+
+
+    file.write(str(grade) + ' ' + str(round(grade.mean(), 2)) + '\n')
+
+file.close()
+
+file = open("KAfterScale.txt", "w")
+file.write(str(k))
+file.close()
